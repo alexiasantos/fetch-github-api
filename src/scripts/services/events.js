bb390866edabf65bb8bad469403a2f89668020ee
Aléxia de Oliveira <alexia.sousa@metasix.com.br>
2025-07-30 21:48:58 -0300

    Adicionando limite de eventos

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,6 +1,6 @@
-import {baseUrl} from '../variables.js';
+import {baseUrl, repositoriesQuantity} from '../variables.js';
 async function getEvents(userName){
-    const response = await fetch(`${baseUrl}/${userName}/events`)
+    const response = await fetch(`${baseUrl}/${userName}/events?per_page=${repositoriesQuantity}`)
     return await response.json();
 }
 
